931fcea3717e4739a55edae44d33369ce7289414
kylemorton5770 <kylemortondevelopment@gmail.com>
2015-06-26 08:06:53 -0500

    change cors

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-	res.setHeader('Access-Control-Allow-Headers', '*');
+//	'X-ACCESS_TOKEN, Access-Control-Allow-Origin, Authorization, Origin,
+//	x-requested-with, Content-Type,
+//	Content-Range, Content-Disposition, Content-Description'
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
 //	res.setHeader('Access-Control-Allow-Headers', '');
 
 
